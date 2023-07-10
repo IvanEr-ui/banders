6659d982fab6c8710846e465ddcfcd80c54ffb33
Ivan <79088192+IvanEr-ui@users.noreply.github.com>
2023-07-10 22:11:06 +0300

    main

--- views/public/js/filter.js
+++ views/public/js/filter.js
@@ -1,14 +1,18 @@
-$(function(){
-   
+$(function () {
+
     let $articles = $('.articles');
     $articles.isotope({
         itemSelector: '.gh-card',
-        layoutMode:'fitRows'
+        layoutMode: 'fitRows'
     })
+    let $listFilter = document.querySelectorAll('.filtration a')
+    console.log($listFilter)
+    $('.filtration a ').on('click', function (e) {
+        $listFilter.forEach($FilterItem => $FilterItem.classList.remove('active'))
+        e.target.classList.add("active");
 
-    $('.filtration a ').on('click',function(e){
         e.preventDefault();
         var filter = $(this).attr('data-filter');
-        $articles.isotope({filter:filter})
+        $articles.isotope({ filter: filter })
     })
 })
